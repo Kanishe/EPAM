fe3d49469a90b2acbcc96da9a232fd51522a97e4
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-11-03 22:45:58 +0300

    start work on utils

---
+++ src/test/java/utils/WebDriverFactory.java
@@ -0,0 +1,21 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+    public static WebDriver createDriver(WebDriverType webDriverType) {
+        switch (webDriverType) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+            case OPERA:
+                WebDriverManager.operadriver().setup();
+            default:
+                return null;
+        }
+    }
+}

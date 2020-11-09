0400da44d3714b463706f440a4de63ba3500702f
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-11-09 20:53:59 +0300

    start creating BaseHooks class

--- src/test/java/utils/BaseHooks.java
+++ src/test/java/utils/BaseHooks.java
@@ -1,7 +1,18 @@
 package utils;
 
+
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
 
 public class BaseHooks {
     protected static WebDriver driver;
+    public static final Logger logger = LogManager.getLogger(BaseHooks.class);
+
+    @BeforeAll
+    public static void setDriver (){
+
+    }
+
 }

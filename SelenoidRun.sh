dd20fdabe0c7dbb3f0244b3d097fd14c0e89340c
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-11-17 23:27:45 +0300

    add Selinoid sh for test in Docker

---
+++ SelenoidRun.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ docker ps --filter "name=^selenoid$" --format '{{.Ports}}'
+ if [ $? -eq 0 ]
+  then
+  echo "Selenoid is already running"
+  exit 666
+  else
+  	echo "Selenoid is already running"
+  	open --background -a Docker
+  	sleep 40
+  	echo "Docker is start"
+  	curl -s https://aerokube.com/cm/bash | bash \
+  	&& cm selenoid start --vnc
+  	echo "selenoid is start"
+  	cm selenoid-ui start
+  	cd /Users/alexandrkanishevskii/IdeaProjects/EPAM/
+  	mvn test
+  	echo "Test EPAM finish"
+  	fi
